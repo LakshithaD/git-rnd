8f04ee0de1032d6aed2ab6f4e69f5505342af24a
Lakshitha Matarage <lnalindanath@gmail.com>
2026-01-09 14:07:38 +0530

    initial project

---
+++ src/main/java/com/git/poc/git_rnd/GitRndApplication.java
@@ -0,0 +1,13 @@
+package com.git.poc.git_rnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitRndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitRndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/git/poc/git_rnd/GitRndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.poc.git_rnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitRndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
