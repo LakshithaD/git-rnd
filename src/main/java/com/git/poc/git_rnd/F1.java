302502e92e7e78b9972d3e0945e0f7ba2b62dd16
Lakshitha Matarage <lnalindanath@gmail.com>
2026-01-09 16:36:15 +0530

    f1 -c2

--- src/main/java/com/git/poc/git_rnd/F1.java
+++ src/main/java/com/git/poc/git_rnd/F1.java
@@ -5,4 +5,8 @@ public class F1 {
     public static void featureOne() {
         System.out.println("Feature One - F1");
     }
+
+    public static void featureTwo() {
+        System.out.println("Feature Two - F1");
+    }
 }
