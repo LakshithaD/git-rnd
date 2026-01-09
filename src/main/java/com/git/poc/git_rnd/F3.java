8316470510ba8c111225a06324c1d241e73f8071
Lakshitha Matarage <lnalindanath@gmail.com>
2026-01-09 16:47:28 +0530

    feature 3 implemented

---
+++ src/main/java/com/git/poc/git_rnd/F3.java
@@ -0,0 +1,10 @@
+package com.git.poc.git_rnd;
+
+public class F3 {
+
+    public static void main(String[] args) {
+        System.out.println("F3 class in git_rnd project");
+        System.out.println("F3 class in git_rnd project");
+        System.out.println("F3 class in git_rnd project");
+    }
+}
